9c31296d9c50bb0e2045df307dc0ca03420dd5bb
engsr6982 <pwxg286704@163.com>
2025-08-16 22:44:04 +0800

    refactor: simplify concepts by removing redundant XYZ checks

--- src/plotx/infra/Concepts.hpp
+++ src/plotx/infra/Concepts.hpp
@@ -6,21 +6,17 @@ namespace plotx {
 
 
 template <typename T>
-concept HasXYZBase_v = requires(T const& t) {
+concept HasXYZ_v = requires(T const& t) {
     { t.x } -> std::convertible_to<int>;
     { t.y } -> std::convertible_to<int>;
     { t.z } -> std::convertible_to<int>;
 };
 
 template <typename T>
-concept HasXYZStyle_v = requires(T const& t) {
-    { t.x_ } -> std::convertible_to<int>;
-    { t.y_ } -> std::convertible_to<int>;
-    { t.z_ } -> std::convertible_to<int>;
+concept HasXZ_v = requires(T const& t) {
+    { t.x } -> std::convertible_to<int>;
+    { t.z } -> std::convertible_to<int>;
 };
-
-template <typename T>
-concept HasXYZ_v = HasXYZBase_v<T> || HasXYZStyle_v<T>;
 
 
 } // namespace plotx
